e6e38c9187eb14b9f792b9382f627155ac409153
mszymutk <mszymutk@redhat.com>
2025-02-26 14:39:16 +0100

    Change argument parser.

--- sbomgrader/__main__.py
+++ sbomgrader/__main__.py
@@ -1,5 +1,6 @@
 import sys
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
+from dataclasses import dataclass
 from pathlib import Path
 
 from rich.console import Console
@@ -16,13 +17,40 @@ from sbomgrader.translate.choose_map import choose_map, get_all_map_list_markdow
 from sbomgrader.translate.translation_map import TranslationMap
 
 
-def grade():
-    parser = ArgumentParser("sbomgrader")
+def _file(arg: str) -> Path:
+    path = Path(arg)
+    if not path.exists():
+        print(f"File {arg} not found.", file=sys.stderr)
+        exit(1)
+    return path
+
+
+@dataclass
+class GradeConfig:
+    input_file: Path
+    cookbook_references: list[str]
+    content_type: SBOMType
+    sbom_type: SBOMTime
+    passing_grade: Grade
+    output_type: OutputType
+
+    @staticmethod
+    def from_args(args: Namespace) -> "GradeConfig":
+        return GradeConfig(
+            input_file=args.input,
+            cookbook_references=args.cookbooks,
+            content_type=args.content_type,
+            sbom_type=args.sbom_type,
+            passing_grade=args.passing_grade,
+            output_type=args.output,
+        )
+
+
+def create_grade_parser(parser: ArgumentParser):
     parser.add_argument(
         "input",
-        type=Path,
+        type=_file,
         help="SBOM File to grade. Currently supports JSON.",
-        nargs="?",
     )
     parser.add_argument(
         "--cookbooks",
@@ -32,13 +60,6 @@ def grade():
         help="Cookbooks to use for validation. "
         "Might reference default cookbooks, directories or files. "
         "Only files with '.yml' or '.yaml' extensions are taken into account if files or directories are specified.",
-    )
-    parser.add_argument(
-        "--list-cookbooks",
-        "-l",
-        action="store_true",
-        default=False,
-        help="List available default cookbooks and exit.",
     )
     parser.add_argument(
         "--content-type",
@@ -70,93 +91,148 @@ def grade():
         help="Specify the output format.",
     )
 
-    args = parser.parse_args()
+
+def grade(config: GradeConfig) -> None:
     console = Console()
-    default_cookbooks = Cookbook.load_all_defaults()
-    if args.list_cookbooks:
-        console.print(Markdown("\n".join(f"- {cb.name}" for cb in default_cookbooks)))
-        exit(0)
 
-    sbom_file = args.input
-    if not sbom_file:
-        print("Please supply an SBOM file.", file=sys.stderr)
-        parser.print_help(sys.stderr)
-        exit(1)
-    doc = Document(get_mapping(sbom_file))
+    doc = Document(get_mapping(config.input_file))
 
-    if args.cookbooks:
-        cookbook_bundle = select_cookbook_bundle(args.cookbooks)
+    if config.cookbook_references:
+        cookbook_bundle = select_cookbook_bundle(config.cookbook_references)
         if not cookbook_bundle.cookbooks:
             print("No cookbook(s) could be found.", file=sys.stderr)
             exit(1)
     else:
         # Cookbooks weren't specified, using defaults
-        type_ = SBOMType(args.content_type)
+        type_ = SBOMType(config.content_type)
         if type_ is SBOMType.UNSPECIFIED:
             type_ = doc.sbom_type
         cookbook_bundle = CookbookBundle.for_document_type(
-            type_, SBOMTime(args.sbom_type)
+            type_, SBOMTime(config.sbom_type)
         )
 
     result = cookbook_bundle(doc)
 
-    output_type = OutputType(args.output)
+    output_type = OutputType(config.output_type)
     if output_type is OutputType.VISUAL:
         markdown = Markdown(result.output(output_type))
         console.print(markdown)
     else:
         console.print(result.output(output_type), output_type.value)
-    if validation_passed(result.grade, Grade(args.passing_grade)):
+    if validation_passed(result.grade, Grade(config.passing_grade)):
         exit(0)
     exit(1)
 
 
-def convert():
-    parser = ArgumentParser("sbomconv")
+@dataclass
+class ConvertConfig:
+    input_file: Path
+    output_format: SBOMFormat
+    custom_maps: list[Path]
+
+    @staticmethod
+    def from_args(args: Namespace) -> "ConvertConfig":
+        return ConvertConfig(
+            input_file=args.input,
+            output_format=args.output_format,
+            custom_maps=args.custom_map,
+        )
+
+
+def create_convert_parser(parser: ArgumentParser):
     parser.add_argument(
         "input",
-        type=Path,
+        type=_file,
         help="SBOM File to convert. Currently supports JSON.",
         nargs="?",
-    )
-    parser.add_argument(
-        "--list-maps",
-        "-l",
-        action="store_true",
-        default=False,
-        help="List available default translation maps and exit.",
     )
     parser.add_argument(
         "--output-format",
         "-f",
         choices=[v.value for v in SBOMFormat],
-        required=not ("--list-maps" in sys.argv or "-l" in sys.argv),
+        required=True,
         help="",
     )
     parser.add_argument(
-        "--custom-map", "-m", type=Path, help="Custom translation map file.", nargs="*"
+        "--custom-map",
+        "-m",
+        type=_file,
+        help="Custom translation map file.",
+        action="append",
     )
-    args = parser.parse_args()
 
-    custom_map_files = args.custom_map or []
-    custom_maps = [TranslationMap.from_file(f) for f in custom_map_files]
 
-    if args.list_maps:
-        console = Console()
-        console.print(Markdown(get_all_map_list_markdown(*custom_maps)))
-        exit(0)
-    inp_file = args.input
-    if not inp_file:
-        print("Please supply an SBOM file.", file=sys.stderr)
-        parser.print_help(sys.stderr)
-        exit(1)
+def convert(config: ConvertConfig) -> None:
+
+    custom_maps = [TranslationMap.from_file(f) for f in config.custom_maps]
+
+    inp_file = config.input_file
     doc = Document(get_mapping(inp_file))
 
-    target_format = SBOMFormat(args.output_format)
+    target_format = SBOMFormat(config.output_format)
 
     t_map = choose_map(doc, target_format, *custom_maps)
     print(t_map.convert(doc, target_format).json_dump)
+    exit(0)
+
+
+@dataclass
+class ListConfig:
+    maps: bool
+    cookbooks: bool
+
+    @staticmethod
+    def from_args(args: Namespace) -> "ListConfig":
+        return ListConfig(maps=args.maps, cookbooks=args.cookbooks)
+
+
+def create_list_parser(parser: ArgumentParser):
+    mutually_exclusive = parser.add_mutually_exclusive_group(required=True)
+    mutually_exclusive.add_argument(
+        "--maps",
+        "-m",
+        action="store_true",
+        default=False,
+        help="List available default translation maps and exit.",
+    )
+    mutually_exclusive.add_argument(
+        "--cookbooks",
+        "-c",
+        action="store_true",
+        default=False,
+        help="List available default cookbooks and exit.",
+    )
+
+
+def list_(config: ListConfig):
+    console = Console()
+    if config.cookbooks:
+        default_cookbooks = Cookbook.load_all_defaults()
+        console.print(Markdown("\n".join(f"- {cb.name}" for cb in default_cookbooks)))
+    if config.maps:
+        console.print(Markdown(get_all_map_list_markdown()))
+    exit(0)
+
+
+def main():
+    parser = ArgumentParser("sbomgrader")
+    subparsers = parser.add_subparsers(required=True, dest="command")
+    grade_parser = subparsers.add_parser("grade")
+    create_grade_parser(grade_parser)
+    convert_parser = subparsers.add_parser("convert")
+    create_convert_parser(convert_parser)
+    list_parser = subparsers.add_parser("list")
+    create_list_parser(list_parser)
+    args = parser.parse_args()
+
+    map_ = {
+        "grade": (grade, GradeConfig),
+        "convert": (convert, ConvertConfig),
+        "list": (list_, ListConfig),
+    }
+    func, config_class = map_[args.command]
+    return func(config_class.from_args(args))
 
 
 if __name__ == "__main__":
-    grade()
+    main()
